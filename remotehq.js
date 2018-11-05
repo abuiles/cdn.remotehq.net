37b53c7872461572c13d5ad3667579b8ec9a4f87
Adolfo Builes <builes.adolfo@gmail.com>
2018-11-05 07:51:39 -0500

    Version bump.

--- remotehq.js
+++ remotehq.js
@@ -1,5 +1,5 @@
 var _remotehqjobs = {
-  version: 1,
+  version: 2,
   css: '.remotehqjob{   display: block;        overflow: hidden;        padding: 1em;        padding-bottom: 0.3em;        font-family: Verdana, "Helvetica Neue", Helvetica, sans-serif;        line-height: 1.5;        border: 4px solid #f5f5f5;        margin-bottom: 10px;    }      .remotehqjob span {        display: block;        overflow: hidden      }      .remotehqjob .image {        display: block;        margin: 0 auto 1em;        margin-left: 25px; max-width: 130px }      .remotehqjob .text {        display: block;        text-align: left;        margin-bottom: 1em;        color: #666;      }.remotehq-poweredby {display: block;    text-align: right;    font-size: .7em;    color: #ccc; }',
   jobs: [
     {
